0744eed630c8f76b67946c09534398de8d5c8211
Luke de Klein <delukeklein@gmail.com>
2019-12-05 16:27:18 +0100

    Add main class

---
+++ UltimateKits/src/com/delukeklein/ultimatekits/UltimateKits.java
@@ -0,0 +1,16 @@
+package com.delukeklein.ultimatekits;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UltimateKits extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
