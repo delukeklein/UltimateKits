1ae7e9c97f0b731b80781a1fff0126e44994fec9
Luke de Klein <delukeklein@gmail.com>
2019-12-06 12:34:59 +0100

    Add LevelKit which extends from AbstractKit

---
+++ UltimateKits/src/com/delukeklein/ultimatekits/kit/LevelKit.java
@@ -0,0 +1,34 @@
+package com.delukeklein.ultimatekits.kit;
+
+import java.util.Map;
+
+import org.bukkit.entity.Player;
+
+import com.delukeklein.ultimatekits.kit.equipment.Equipment;
+
+public final class LevelKit extends AbstractKit {
+	
+	private final Map<Level, Equipment> levels;
+	
+	@SuppressWarnings("unchecked")
+	public LevelKit(final Map<String, Object> map) {
+		super(map);
+		
+		this.levels = (Map<Level, Equipment>) map.get("levels");
+	}
+	
+	@Override
+	public void giveKit(final Player player) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public Map<String, Object> serialize() {
+		final Map<String, Object> map = super.serialize();
+		
+		map.put("levels", levels);
+		
+		return map;
+	}
+
+}
