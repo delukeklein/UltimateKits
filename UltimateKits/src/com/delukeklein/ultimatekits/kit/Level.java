3c1d3a76ec54d17f29f5c6480c7d8b7596166494
Luke de Klein <delukeklein@gmail.com>
2019-12-06 12:35:11 +0100

    Add level object

---
+++ UltimateKits/src/com/delukeklein/ultimatekits/kit/Level.java
@@ -0,0 +1,25 @@
+package com.delukeklein.ultimatekits.kit;
+
+public final class Level {
+
+	private int price;
+	
+	private String name;
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
