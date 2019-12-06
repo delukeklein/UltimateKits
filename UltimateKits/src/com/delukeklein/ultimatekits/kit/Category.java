77c4fa8719e2d70ea00b57bdfd5c23d9ce4f51dc
Luke de Klein <delukeklein@gmail.com>
2019-12-06 12:33:54 +0100

    Add kit category object

---
+++ UltimateKits/src/com/delukeklein/ultimatekits/kit/Category.java
@@ -0,0 +1,19 @@
+package com.delukeklein.ultimatekits.kit;
+
+public final class Category {
+
+	private String name;
+	
+	public Category(final String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+	
+}
