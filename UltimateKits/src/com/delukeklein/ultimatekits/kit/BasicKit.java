4f91f6dd39e739b4712dc54a1fd5f8d35e483095
Luke de Klein <delukeklein@gmail.com>
2019-12-07 15:37:25 +0100

    Remove unnecessary abstraction

--- UltimateKits/src/com/delukeklein/ultimatekits/kit/BasicKit.java
+++ UltimateKits/src/com/delukeklein/ultimatekits/kit/BasicKit.java
@@ -1,5 +1,59 @@
 package com.delukeklein.ultimatekits.kit;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
 import com.delukeklein.ultimatekits.kit.equipment.Equipment;
 
-public interface BasicKit extends Kit, Equipment { }
+public final class BasicKit extends AbstractKit implements Equipment {
+	
+	private ItemStack[] armor;
+	private ItemStack[] storage;
+	
+	@SuppressWarnings("unchecked")
+	public BasicKit(final Map<String, Object> map) {
+		super(map);
+		
+		armor = ((List<ItemStack>) map.get("armor")).toArray(armor);
+		storage = ((List<ItemStack>) map.get("storage")).toArray(storage);
+	}
+	
+	@Override
+	public void giveKit(Player player) {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void setArmor(ItemStack[] armor) {
+		this.armor = armor;	
+	}
+
+	@Override
+	public void setStorage(ItemStack[] storage) {
+		this.storage = storage;	
+	}
+	
+	@Override
+	public ItemStack[] getArmor() {
+		return armor;
+	}
+
+	@Override
+	public ItemStack[] getStorage() {
+		return storage;
+	}
+	
+	@Override
+	public Map<String, Object> serialize() {
+		final Map<String, Object> map = super.serialize();
+
+		map.put("armor", Arrays.asList(armor));
+		map.put("storage", Arrays.asList(storage));
+		
+		return map;
+	}
+}
--- UltimateKits/src/com/delukeklein/ultimatekits/kit/LevelKit.java
+++ UltimateKits/src/com/delukeklein/ultimatekits/kit/LevelKit.java
@@ -1,9 +1,40 @@
 package com.delukeklein.ultimatekits.kit;
 
-public interface LevelKit extends Kit {
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.entity.Player;
+
+final class LevelKit extends AbstractKit {
 	
-	void addLevel(Level level);
+	private final List<Level> levels;
 	
-	boolean removeLevel(String levelName);
+	@SuppressWarnings("unchecked")
+	public LevelKit(final Map<String, Object> map) {
+		super(map);
+		
+		this.levels = (List<Level>) map.get("levels");
+	}
 	
+	@Override
+	public void giveKit(final Player player) {
+		
+	}
+	
+	public void addLevel(Level level) {
+		levels.add(level);
+	}
+	
+	public boolean removeLevel(String levelName) {
+		return levels.removeIf(l -> l.getName() == levelName);
+	}
+
+	@Override
+	public Map<String, Object> serialize() {
+		final Map<String, Object> map = super.serialize();
+		
+		map.put("levels", levels);
+		
+		return map;
+	}
 }
